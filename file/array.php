3d95ab60a8934f780ded5112fd91408925586b3a
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-09 00:22:43 +0700

    Change README

---
+++ file/array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+</head>
+<body>
+    <?php 
+        $books = [
+            "Do Androids Dream of Electric Sheep?",
+            "The Langoliers",
+            "Hail Mary"
+        ];
+    ?>
+
+    <ul>
+        <?php foreach($books as $book): ?>
+            <li><?= $book; ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
