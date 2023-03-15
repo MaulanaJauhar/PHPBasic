5f08e5fab21fdd7634a4c8f723ce6e98e98b6934
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-15 20:53:05 +0700

    Changes Index.php

--- file/index.php
+++ file/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
-</head>
-<body>
-    <?php 
+<?php 
         $books = [
             [
               'name'          => "Do Andorids Dream of Electric Sheep",
@@ -33,7 +24,15 @@
               return $book['realiseYear'] >= 1950 && $book['realiseYear'] <= 2000;
           });
     ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+<body>
     <ul>
         <?php foreach ($filteredBooks as $book) : ?>
             <li>
