3e605b5c64edf302cbacae89021a4af98eded587
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-15 20:56:29 +0700

    Add Index.view.php

--- file/index.php
+++ file/index.php
@@ -1,46 +1,27 @@
 <?php 
-        $books = [
-            [
-              'name'          => "Do Andorids Dream of Electric Sheep",
-              'author'        => "Philip K. Dick",
-              'realiseYear'   => 1968,
-              'purchase_url'  => "http://example.com"
-            ],
-            [
-              'name'          => "Project Hail Mary",
-              'author'        => "Andy Weir",
-              'realiseYear'   => 2021,
-              'purchase_url'  => "http://example.com"
-            ],
-            [
-                'name'          => "The Martisan",
-                'author'        => "Andy Weir",
-                'realiseYear'   => 2011,
-                'purchase_url'  => "http://example.com"
-            ]
-          ];
+    $books = [
+        [
+            'name'          => "Do Andorids Dream of Electric Sheep",
+            'author'        => "Philip K. Dick",
+            'realiseYear'   => 1968,
+            'purchase_url'  => "http://example.com"
+        ],
+        [
+            'name'          => "Project Hail Mary",
+            'author'        => "Andy Weir",
+            'realiseYear'   => 2021,
+            'purchase_url'  => "http://example.com"
+        ],
+        [
+            'name'          => "The Martisan",
+            'author'        => "Andy Weir",
+            'realiseYear'   => 2011,
+            'purchase_url'  => "http://example.com"
+        ]
+        ];
 
-          $filteredBooks = array_filter($books, function($book){
-              return $book['realiseYear'] >= 1950 && $book['realiseYear'] <= 2000;
-          });
-    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
-</head>
-<body>
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['purchase_url'] ?>">
-                    <?= $book['name']; ?> (<?= $book['realiseYear'] ?>) - By <?= $book['author'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?> 
-    </ul>
-</body>
-</html>
+    $filteredBooks = array_filter($books, function($book){
+        return $book['realiseYear'] >= 1950 && $book['realiseYear'] <= 2000;
+});
+
+require "index.view.php";
