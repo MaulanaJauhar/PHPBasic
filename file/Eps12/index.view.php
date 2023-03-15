780d8d4e32bd7382c05908af1013e1ad15003bed
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-15 23:30:29 +0700

    Add Main Content

--- file/Eps12/index.view.php
+++ file/Eps12/index.view.php
@@ -143,7 +143,7 @@
   </header>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
+      <p>Hello. Welcome to the Home Page</p>
     </div>
   </main>
 </div>
