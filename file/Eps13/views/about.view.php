a08bf0bbbd7728a7b705d27785c4ecaf09e25318
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-16 10:31:45 +0700

    Change File

--- file/Eps13/views/about.view.php
+++ file/Eps13/views/about.view.php
@@ -8,14 +8,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
 <div class="min-h-full">
 
   <header class="bg-white shadow">
