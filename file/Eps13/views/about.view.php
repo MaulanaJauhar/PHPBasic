d2fe23d2d22b30a3f588ebdbfddaa9b2f3782b54
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-16 10:36:15 +0700

    Changes Content

--- file/Eps13/views/about.view.php
+++ file/Eps13/views/about.view.php
@@ -8,18 +8,18 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
-<div class="min-h-full">
-
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+    <div class="min-h-full">
+        <?php require 'partials/nav.php';?>
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <p>Hello. Welcome to the About Page</p>
+            </div>
+        </main>
     </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <p>Hello. Welcome to the About Page</p>
-    </div>
-  </main>
-</div>
 </body>
 </html>
