ad6146d05830bddf3b52c12bfcf9b8e3aa627317
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-16 10:39:19 +0700

    Changes Partials

--- file/Eps13/views/index.view.php
+++ file/Eps13/views/index.view.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full">
-    <div class="min-h-full">
+<?php require 'partials/header.php';?>
         <?php require 'partials/nav.php';?>
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
             </div>
         </header>
         <main>
@@ -20,6 +10,4 @@
                 <p>Hello. Welcome to the Home Page</p>
             </div>
         </main>
-    </div>
-</body>
-</html>
+<?php require 'partials/footer.php';?>
